2a79d250c48581eae2a1f7350381dec916ea6ff1
Marcin <asssinowo90@gmail.com>
2024-04-07 19:35:44 +0200

    added communication.tf

---
+++ Terraform/communication.tf
@@ -0,0 +1,5 @@
+﻿resource "azurerm_communication_service" "example" {
+  name                = "socialplatformcs"
+  resource_group_name = azurerm_resource_group.project_engineers.name
+  data_location       = "Europe"
+}
--- Terraform/database.tf
+++ Terraform/database.tf
@@ -1,35 +1,32 @@
-﻿resource "azurerm_postgresql_server" "example" {
+﻿resource "azurerm_postgresql_flexible_server" "example" {
   depends_on = [azurerm_resource_group.project_engineers]
-  name                = "socialplatforms"
-  location            = azurerm_resource_group.project_engineers.location
+  name                = "socialplatformser"
+  location            = "polandcentral"
   resource_group_name = azurerm_resource_group.project_engineers.name
-  sku_name            = "B_Gen5_1"
+  sku_name            = "B_Standard_B1ms"
   version             = "11"
-  ssl_enforcement_enabled = true
   administrator_login          = "marcin"
-  administrator_login_password = var.password
+  administrator_password = var.password
 
-    storage_mb            = 5120  # 5GB
+    storage_mb            = 32768  # 5GB
     backup_retention_days = 7
     geo_redundant_backup_enabled  = false
 }
 
 # Create PostgreSQL Database
-resource "azurerm_postgresql_database" "example" {
-  depends_on = [azurerm_postgresql_server.example]
+resource "azurerm_postgresql_flexible_server_database" "example" {
+  depends_on = [azurerm_postgresql_flexible_server.example]
+  server_id = azurerm_postgresql_flexible_server.example.id
   name                = "socialplatformdb"
-  resource_group_name = azurerm_resource_group.project_engineers.name
-  server_name         = azurerm_postgresql_server.example.name
   charset             = "UTF8"
-  collation           = "English_United States.1252"
+  collation           = "en_US.utf8"
 }
 
 # Grant access to the PostgreSQL Server from Web App
-resource "azurerm_postgresql_firewall_rule" "example" {
-  depends_on = [azurerm_linux_web_app.example]
+resource "azurerm_postgresql_flexible_server_firewall_rule" "example" {
+  depends_on = [azurerm_postgresql_flexible_server.example]
   name                = "allow-webapp-access"
-  resource_group_name = azurerm_resource_group.project_engineers.name
-  server_name         = azurerm_postgresql_server.example.name
+  server_id = azurerm_postgresql_flexible_server.example.id
   start_ip_address   = azurerm_linux_web_app.example.outbound_ip_address_list[0]
   end_ip_address     = azurerm_linux_web_app.example.outbound_ip_address_list[0]
 }
