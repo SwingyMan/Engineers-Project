5d9cdcac06d5b9d2e402bb6386ff93e914f9f961
SwingyMan <asssinowo90@gmail.com>
2024-05-01 00:37:45 +0200

    added new services

--- Terraform/qnamaker.tf
+++ Terraform/qnamaker.tf
@@ -6,3 +6,19 @@ resource "azurerm_cognitive_account" "example" {
   sku_name            = "S"
   kind                = "TextAnalytics"
 }
+resource "azurerm_cognitive_account" "moderator" {
+  depends_on          = [azurerm_resource_group.project_engineers]
+  name                = "socialplatformmd"
+  location            = azurerm_resource_group.project_engineers.location
+  resource_group_name = azurerm_resource_group.project_engineers.name
+  sku_name            = "S"
+  kind                = "ContentModerator"
+}
+resource "azurerm_cognitive_account" "translator" {
+  depends_on          = [azurerm_resource_group.project_engineers]
+  name                = "socialplatformtr"
+  location            = azurerm_resource_group.project_engineers.location
+  resource_group_name = azurerm_resource_group.project_engineers.name
+  sku_name            = "S"
+  kind                = "TextTranslation"
+}
