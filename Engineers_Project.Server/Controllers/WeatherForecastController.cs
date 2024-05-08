eeb04f1393e120e152d3f9754c980be57c67f919
Marcin <asssinowo90@gmail.com>
2024-05-08 19:25:44 +0200

    fixed injects

--- Engineers_Project.Server/Controllers/WeatherForecastController.cs
+++ Engineers_Project.Server/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using Infrastructure.ContentSafety;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -10,17 +11,25 @@ namespace Engineers_Project.Server.Controllers
 	public class WeatherForecastController : ControllerBase
 	{
 		private readonly IEmailSender _emailSender;
+		private readonly TextContentSafetyService _textContentSafety;
 
-
-		public WeatherForecastController(IEmailSender emailSender)
+		public WeatherForecastController(IEmailSender emailSender,TextContentSafetyService textContentSafetyService)
 		{
 			_emailSender = emailSender;
+			_textContentSafety = textContentSafetyService;
 		}
 		[HttpGet("/api/email/send")]
 		public IActionResult send(string email,string message,string subject)
 		{
 			_emailSender.SendEmailAsync(email, subject, message);
 			return Ok();
+		}
+
+		[HttpGet("/api/text/test")]
+		public IActionResult test(string text)
+		{
+			var test = _textContentSafety.AnalyzeText(text);
+			return Ok(test);
 		}
 	}
 }
