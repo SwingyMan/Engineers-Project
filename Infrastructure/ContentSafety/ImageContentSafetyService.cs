a0e6b53c5283d247263e941faded9ecd5f49cd46
Tomasz Kalus <tomaszkalus@interia.pl>
2024-05-08 18:54:32 +0200

    Added services for image and text content moderation

---
+++ Infrastructure/ContentSafety/ImageContentSafetyService.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Infrastructure.ContentSafety
+{
+    public class ImageContentSafetyService
+    {
+        public Response<AnalyzeImageResult> AnalyzeImage(string imagePath)
+        {
+            var keyvault = new SecretClient(new Uri("https://socialplatformkv.vault.azure.net/"), new DefaultAzureCredential());
+            string endpoint = "https://westeurope.api.cognitive.microsoft.com/";
+            string key = keyvault.GetSecret("storagekey").Value.Value;
+            ContentSafetyClient client = new ContentSafetyClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+            ContentSafetyImageData image = new ContentSafetyImageData(BinaryData.FromBytes(File.ReadAllBytes(imagePath)));
+
+            var request = new AnalyzeImageOptions(image);
+
+            Response<AnalyzeImageResult> response;
+            try
+            {
+                response = client.AnalyzeImage(request);
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine("Analyze image failed.\nStatus code: {0}, Error code: {1}, Error message: {2}", ex.Status, ex.ErrorCode, ex.Message);
+                throw;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Infrastructure/ContentSafety/TextContentSafetyService.cs
@@ -0,0 +1,32 @@
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Infrastructure.ContentSafety
+{
+    public class TextContentSafetyService
+    {
+        public Response<AnalyzeTextResult> AnalyzeText(string text)
+        {
+            var keyvault = new SecretClient(new Uri("https://socialplatformkv.vault.azure.net/"), new DefaultAzureCredential());
+            string endpoint = "https://westeurope.api.cognitive.microsoft.com/";
+            string key = keyvault.GetSecret("storagekey").Value.Value;
+            ContentSafetyClient client = new ContentSafetyClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+            var request = new AnalyzeTextOptions(text);
+
+            Response<AnalyzeTextResult> response;
+            try
+            {
+                response = client.AnalyzeText(request);
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine("Analyze text failed.\nStatus code: {0}, Error code: {1}, Error message: {2}", ex.Status, ex.ErrorCode, ex.Message);
+                throw;
+            }
+            return response;
+        }
+    }
+}
