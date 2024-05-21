2da77e0617cebea2d16d20a84504494fff6d9f64
Tomasz Kalus <tomaszkalus@interia.pl>
2024-05-21 19:02:06 +0200

    Added translator service class and necessary DTOs

---
+++ Infrastructure/Translator/TranslatorDTO/DetectedLanguage.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Translator.TranslatorDTO;
+public class DetectedLanguage
+{
+    public string language { get; set; }
+    public double score { get; set; }
+}
---
+++ Infrastructure/Translator/TranslatorDTO/Translation.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Translator.TranslatorDTO;
+
+public class Translation
+{
+    public string text { get; set; }
+    public string to { get; set; }
+}
