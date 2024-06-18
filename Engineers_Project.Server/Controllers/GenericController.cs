da952c64bd39c07c5e617e8f7e35b2e3d0a3774d
Tomasz Kalus <tomaszkalus@interia.pl>
2024-06-18 19:08:57 +0200

    Added PostController

---
+++ Engineers_Project.Server/Controllers/GenericController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Application.Queries;
+using Application.Commands;
+
+namespace Engineers_Project.Server.Controllers;
+
+[ApiController]
+[GenericRestControllerNameConvention]
+[Route("[controller]")]
+public class GenericController<T, TRequest, TResponse> : ControllerBase where T : class where TRequest : class where TResponse : class
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public GenericController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<TResponse>> Get(Guid key)
+    {
+        var query = new GenericGetByIdQuery<T>(key);
+        var result = await _mediator.Send(query);
+        if (result == null) return NotFound();
+        return _mapper.Map<T, TResponse>(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TResponse>> Create(TRequest request)
+    {
+        var command = new GenericAddCommand<TRequest, TResponse>(request);
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
