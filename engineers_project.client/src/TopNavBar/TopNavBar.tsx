75efae0ef86a89aaf187df23fd6f94c96691e650
speed150 <97861636+speed150@users.noreply.github.com>
2024-06-24 14:25:13 +0200

    navBar start

---
+++ engineers_project.client/src/TopNavBar/TopNavBar.tsx
@@ -0,0 +1,14 @@
+import styles from "../TopNavBar/TopNavBar.module.css"
+
+export function TopNavBar(){
+    return<>
+    <div id={styles.topNavBar}>
+        <div id={styles.Logo}>
+            <img height={40} width={40} src="src/assets/logo-politechnika.png"/>
+        </div>
+        <div id={styles.SearchBar}></div>
+    </div>
+    
+    
+    </>
+}
