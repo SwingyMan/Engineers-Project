d1a9d6330ea02ca6f801baf77fb75a143d2b2d29
Tomasz Kalus <tomakal079@student.polsl.pl>
2024-12-15 22:36:23 +0100

    Added endpoint for retrieving all chats that user is a member of in chat controller

---
+++ Application/Queries/GetChatsByUserIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Queries;
+
+public class GetChatsByUserIdQuery : IRequest<IEnumerable<Chat>>
+{
+    public GetChatsByUserIdQuery(Guid guid)
+    {
+        userGuid = guid;
+    }
+
+    public Guid userGuid;
+}
---
+++ Application/Queries/GetChatsByUserIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Infrastructure.IRepositories;
+using MediatR;
+
+namespace Application.Queries;
+
+public class GetChatsByUserIdQueryHandler : IRequestHandler<GetChatsByUserIdQuery, IEnumerable<Chat>>
+{
+    private readonly IChatRepository _chatRepository;
+
+    public GetChatsByUserIdQueryHandler(IChatRepository chatRepository)
+    {
+        _chatRepository = chatRepository;
+    }
+
+    public Task<IEnumerable<Chat>> Handle(GetChatsByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        return _chatRepository.GetChatsByUserId(request.userGuid);
+    }
+}
