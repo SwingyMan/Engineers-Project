026e0cd2b287b355c4f38149c8cdcc5fb0f84b0c
speed150 <pawel.c95311@gmail.com>
2024-12-19 19:52:09 +0100

    fix

--- engineers_project.client/src/Pages/FeedPage.tsx
+++ engineers_project.client/src/Pages/FeedPage.tsx
@@ -57,7 +57,7 @@ export function FeedPage() {
     }
     else{
       const action = event.target.getAttribute("data-action")
-      const
+      const id = event.target.getAttributes("data-id")
     }
   };
 
