3ba190c72efa19c860d2bf1ba295791eef7a1e74
Marcin <asssinowo90@gmail.com>
2025-01-08 22:34:30 +0100

    added friends dto

---
+++ Application/DTOs/FriendsDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Application.DTOs;
+
+public class FriendsDTO
+{
+    public List<User> Sent { get; set; } 
+    public List<User> Received { get; set; }
+    public List<User> Friends { get; set; }
+
+    public FriendsDTO(List<User> sent, List<User> received, List<User> friends)
+    {
+        Sent = sent;
+        Received = received;
+        Friends = friends;
+    }
+}
